f4f5e3fd1518ad2b9adbad81bd268bb21ce55737
Dekoder-py <kyleyesmariam@icloud.com>
2025-04-24 16:31:01 +1200

    add addition function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,11 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+	const result = num1 + num2;
+	alert(`The result is ${result}`)
+}
+
 let calculationDescription = `(${currentResult} + 10) * 3 / 2 - 1`;
 
 currentResult = (currentResult + 10) * 3 / 2 - 1;
